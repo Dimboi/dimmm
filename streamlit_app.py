0e1d67b4bc1ce20fceaaf90d723746edcdfe24a9
Dimboi <dimas.catur.winson@gmail.com>
2024-06-24 11:00:55 +0700

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+import stream as st
+
+st.title('chance orang idup pas kena jantung(yes/no)')
+umur = st.text_input ('umur anda: ')
+anemia = st.text_input ('apakah anda anemia: ')
+ceratine = st.text_input ('kandungan ceratine anda: ')
+diabetes = st.text_input ('kandungan diabetes anda: ')
